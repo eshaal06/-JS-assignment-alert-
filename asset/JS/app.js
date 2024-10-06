1b7c401bb4122479a397affa61c52cb726821b6c
eshaal06 <esh44l99@gmail.com>
2024-10-06 10:34:19 +0500

    alert

---
+++ asset/JS/app.js
@@ -0,0 +1,18 @@
+// question no 1
+// alert("Welcome to our website!");
+
+// question no 2
+// alert("Error! Please enter a valid password.")
+
+// question no 3
+// alert("Welcome to JS land\nHappy coding!")
+
+// question no 4
+// alert("Welcome to our website!\nHave a great day!");
+// alert("Enjoy exploring our content!");    
+
+// question no 5
+// (This code has been generated in browser's console)
+
+// question no 6
+    alert("Welcome to our website!!")
